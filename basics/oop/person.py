2f9c43ef94ec0f66aca701f60697e9c9d7edf299
paciadawid <paciadawid@gmail.com>
2022-12-05 20:21:49 +0100

    oop + unittest

--- basics/oop/person.py
+++ basics/oop/person.py
@@ -19,8 +19,38 @@ class Person:
         self.surname = surname
 
 
-stefan = Person("Stefan", "Testowy", 1999)
-stefan.print_full_name()
-print(stefan.get_age())
-stefan.change_surname("Nietestowy")
-stefan.print_full_name()
+class Employee(Person):
+
+    def __init__(self, name, surname, year_of_birth, type_of_employment):
+        super().__init__(name, surname, year_of_birth)
+        self.type_of_employment = type_of_employment
+
+    def change_employment_type(self, new_type):
+        self.type_of_employment = new_type
+
+
+class Student(Person):
+
+    def __init__(self, name, surname, year_of_birth, initial_grades=None):
+        super().__init__(name, surname, year_of_birth)
+        if initial_grades is None:
+            initial_grades = []
+        self.initial_grades = initial_grades
+
+    def add_grade(self, grade):
+        self.initial_grades.append(grade)
+
+    def calculate_grade(self):
+        grade = round(sum(self.initial_grades) / len(self.initial_grades))
+        return grade
+
+
+if __name__ == "__main__":
+    andrzej_programmer = Employee("Andrzej", "Programmer", 1999, "B2B")
+    andrzej_programmer.print_full_name()
+
+    stefan = Person("Stefan", "Testowy", 1999)
+    stefan.print_full_name()
+    print(stefan.get_age())
+    stefan.change_surname("Nietestowy")
+    stefan.print_full_name()
