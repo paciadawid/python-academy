4d48376587fe9b5f9f1330637b20bb24cc63048a
paciadawid <paciadawid@gmail.com>
2022-12-15 20:08:42 +0100

    ip tests

---
+++ api_tests/ip_test.py
@@ -0,0 +1,15 @@
+import unittest
+
+import requests
+
+
+class TestIP(unittest.TestCase):
+
+    def test_my_ip(self):
+        response_body = requests.get("https://api.ipify.org/?format=json").json()
+        self.assertTrue(response_body["ip"])
+        # self.assertRegex(response_body["ip"], "[0-9]{4}")
+
+
+if __name__ == '__main__':
+    unittest.main()
