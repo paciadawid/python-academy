fb81cb7d5e612284e31fd27ce0e1af5babea828e
paciadawid <paciadawid@gmail.com>
2022-12-22 18:38:08 +0100

    api handler

--- api_tests/test_pokeapi.py
+++ api_tests/test_pokeapi.py
@@ -2,12 +2,13 @@ import unittest
 
 import requests
 
+from api_tests.api_handler import APIHandler
+
 
 class TestPokemons(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.url = "https://pokeapi.co/api/v2"
-        self.pokemon_endpoint = "/pokemon"
+        self.api_handler = APIHandler()
 
     def test_default_pokemons(self):
         response = requests.get(self.url + self.pokemon_endpoint)
@@ -17,7 +18,18 @@ class TestPokemons(unittest.TestCase):
         self.assertEqual(200, response.status_code)
         self.assertEqual(1154, response_body["count"])
         self.assertLess(response_time, 1000)
-        self.assertLess(len(response.content), 100*1000)
+        self.assertLess(len(response.content), 100 * 1000)
+
+    def test_pokemons_with_limit_and_offset(self):
+        params = {
+            "limit": 10,
+            "offset": 20
+        }
+        response_body = self.api_handler.get_pokemons(name_or_id="pikachu")
+
+        self.assertEqual(params["limit"], len(response_body["results"]))
+        self.assertRegex(response_body["results"][0]["url"], f"/{params['offset'] + 1}")
+        self.assertIn(f"/{params['offset'] + 1}", response_body["results"][0]["url"])
 
 
 if __name__ == '__main__':
