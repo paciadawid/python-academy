b9f211c8f602d6b4474094048f29d6d1967f4a59
paciadawid <paciadawid@gmail.com>
2023-02-09 20:00:29 +0100

    Appium test

--- mobile/first_run.py
+++ mobile/first_run.py
@@ -1,5 +1,6 @@
 import os
 from appium import webdriver
+from appium.webdriver.common.appiumby import AppiumBy
 
 # Returns abs path relative to this file and not cwd
 app_name = "calculator.apk"
@@ -8,5 +9,6 @@ app_path = os.path.abspath(os.path.join(os.path.dirname(__file__), app_name))
 desired_caps = {}
 desired_caps['platformName'] = 'Android'
 desired_caps['app'] = app_path
+#["fullReset"] = True
 
 driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
