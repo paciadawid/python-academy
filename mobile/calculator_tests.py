328c873ef1a7cac3ec9055fd172db3b78a220589
paciadawid <paciadawid@gmail.com>
2023-02-13 19:28:40 +0100

    Appium extend functions

--- mobile/calculator_tests.py
+++ mobile/calculator_tests.py
@@ -10,9 +10,14 @@ class MyTestCase(unittest.TestCase):
         self.driver = create_driver("Android", "calculator.apk")
         self.home_page = HomePage(self.driver)
 
-    def test_something(self):
+    def test_add_digits(self):
         self.home_page.add_values(3, 5)
         self.assertEqual(self.home_page.get_result(), 8)
+
+    def test_sinus_pi(self):
+        self.home_page.calculate_sinus("pi", "RAD")
+        self.assertEqual(self.home_page.get_result(), 0)
+
 
     def tearDown(self) -> None:
         self.driver.quit()
--- mobile/screens/home.py
+++ mobile/screens/home.py
@@ -8,6 +8,10 @@ class HomePage(BasePage):
     plus_button_selector = (AppiumBy.ACCESSIBILITY_ID, "plus")
     equals_button_selector = (AppiumBy.ACCESSIBILITY_ID, "equals")
     result_field_selector = (AppiumBy.ID, "result_final")
+    expand_button_selector = (AppiumBy.ACCESSIBILITY_ID, "Expand")
+    sinus_button_selector = (AppiumBy.ACCESSIBILITY_ID, "sine")
+    rad_deg_mode_button_selector = (AppiumBy.ID, "toggle_mode")
+    pi_button_selector = (AppiumBy.ACCESSIBILITY_ID, "pi")
 
     # dynamic selectors
     digit_button_selector = (AppiumBy.ID, "digit_{argument}")
@@ -20,3 +24,20 @@ class HomePage(BasePage):
 
     def get_result(self):
         return float(self.driver.find_element(*self.result_field_selector).text)
+
+    def calculate_sinus(self, value, mode="DEG"):
+        self.click(self.expand_button_selector)
+        self.click(self.sinus_button_selector)
+
+        element = self.driver.find_element(*self.rad_deg_mode_button_selector)
+        if element.text == mode:
+            element.click()
+
+        if value == "pi":
+            self.click(self.pi_button_selector)
+        elif value == "e":
+            pass
+        else:
+            self.click(self.parse_dynamic_selector(self.digit_button_selector, value))
+
+        self.click(self.equals_button_selector)
