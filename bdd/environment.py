1ea107a28e5a74f8917aed93833610f450ebceb7
paciadawid <paciadawid@gmail.com>
2023-03-02 19:08:46 +0100

    headless docker run

--- bdd/environment.py
+++ bdd/environment.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 
 from ui_tests.pages.cart import CartPage
 from ui_tests.pages.home import HomePage
@@ -12,7 +13,11 @@ import allure
 
 
 def before_scenario(context, scenario):
-    context.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument(" --disable-dev-shm-usage")
+    context.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), chrome_options=options)
     context.driver.implicitly_wait(5)
     context.driver.get("https://automationexercise.com/")
 
